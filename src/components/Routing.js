211cde7cad0ceed8e6f8065be78152d658c75b30
yuki <mansie.torsholt@gmail.com>
2021-04-06 17:40:11 +0200

    create selection page and call page

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Route, Switch, Link } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 import GuestForm from "./GuestForm"
 import WelcomePage from "./WelcomePage"
+import SelectionPage from "./SelectionPage"
+import CallPage from "./CallPage"
 
 const Routing = (props) => {
     return (
         <Switch>
             <Route exact path="/guest" render={() => <GuestForm {...props} />} />
+            <Route exact path="/call" render={() => <CallPage {...props} />} /> 
+            <Route exact path="/select" render={()=> <SelectionPage {...props} />} />
             <Route exact path="/" component={WelcomePage} />
         </Switch>
     )
