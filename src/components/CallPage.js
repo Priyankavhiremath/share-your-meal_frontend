e72b7da7cd3d8a859b1b70e761ba35602cd514fa
yuki <mansie.torsholt@gmail.com>
2021-04-08 17:31:28 +0200

    create call page

--- src/components/CallPage.js
+++ src/components/CallPage.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
-const CallPage = () => {
+const CallPage = ({ acceptedCall, partnerVideo, connected, userVideo }) => {
     return (
         <div>
             <h1>Hello from the call page</h1>
+            
+            {connected && (
+                <video
+                    style={{ width: "15rem", height: "15rem", borderRadius: "50%", "object-fit": "cover", border: "0.2rem solid white"}}
+                    playsInline
+                    muted
+                    ref={userVideo} //is not displayed
+                    autoPlay
+                    name="userVideo"
+                ></video>
+            )}
+            {acceptedCall && (
+                <video
+                style={{ width: "80%", height: "80%" }}
+                playsInline
+                ref={partnerVideo}
+                autoPlay
+                name="partnerVideo"
+                ></video>)}
         </div>
     )
 }
