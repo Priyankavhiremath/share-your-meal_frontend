1cc1491b41e9a8f106b0fce1a15415ef934d42a7
Priyankavhiremath <priyankahiremath221b@gmail.com>
2021-04-12 23:20:04 +0200

    auth fixed fe

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { userContext } from "../utils/auth";
+import { Button } from "react-bootstrap";
 
 const Profile = ({ me, onLogout }) => {
   const [user, setUser] = useState();
@@ -9,7 +10,7 @@ const Profile = ({ me, onLogout }) => {
       try {
         const { data: userData } = await userContext();
         if (!userData) return onLogout();
-        console.log({ userData });
+        // console.log({ userData });
         setUser(userData);
       } catch (e) {
         onLogout(); // bypassing this just to see the cookie
@@ -28,12 +29,12 @@ const Profile = ({ me, onLogout }) => {
           <div className="row mt-0">
             <div className="col-md-6 m-auto">
               <div className="card card-body bg-light border border-0 shadow m-5">
-                <h1 className="text-center mb-3">Hello {me.name}</h1>
+                <h1 className="text-center mb-3">Hello {user.nickName}</h1>
                 <p>Choose what would you like to change</p>
 
                 <form>
                   <div className="form-group">
-                    <p>Your name is {me.name} </p>
+                    {/* <p>Your name is {user.nickName} </p> */}
                     <label htmlFor="name">Change your name</label>
                     <input
                       type="text"
@@ -45,7 +46,7 @@ const Profile = ({ me, onLogout }) => {
                   </div>
 
                   <div className="form-group">
-                    <p>Your country is {me.country} </p>
+                    {/* <p>Your country is {user.location.country} </p> */}
                     <label htmlFor="country">Change your country</label>
                     <input
                       type="country"
@@ -57,7 +58,7 @@ const Profile = ({ me, onLogout }) => {
                   </div>
 
                   <div className="form-group">
-                    <p>Your country is {me.language} </p>
+                    {/* <p>Your language is {user.languages} </p> */}
                     <label htmlFor="language">Change your language</label>
                     <input
                       type="language"
@@ -67,6 +68,12 @@ const Profile = ({ me, onLogout }) => {
                       placeholder="Enter Language"
                     />
                   </div>
+                  <p>
+                    <Button className="roundButton" onClick={onLogout}>
+                      Logout
+                    </Button>
+                    <br />
+                  </p>
                 </form>
               </div>
             </div>
