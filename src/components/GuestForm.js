4c9dc89ddff36b3408b725c5e0afc54f124f1745
yuki <mansie.torsholt@gmail.com>
2021-04-19 15:42:04 +0200

    add empty form warning on guestform and prevent submit

--- src/components/GuestForm.js
+++ src/components/GuestForm.js
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 import { Button } from "react-bootstrap";
 import "../styles/GuestForm.css";
 
-const GuestForm = ({ onConnect, onChangeForm, setMe, me }) => {
+const GuestForm = ({ onConnect, onChangeForm, setMe, me,formDemandsInfo }) => {
   const languagelist = [
     {
       value: 1,
@@ -136,6 +136,9 @@ const GuestForm = ({ onConnect, onChangeForm, setMe, me }) => {
                       />
                       <label>Both </label>
                     </div>
+                    {formDemandsInfo && (
+                      <h3 className="formEmptyAlert pt-4">Please complete the form before submitting!</h3>
+                    )}
                     <Button className="roundButton mr-md-4" href="/">
                       {" "}
                       Go back{" "}
